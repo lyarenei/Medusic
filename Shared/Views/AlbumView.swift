793bb97cce336ac645c26f64f8e669e7c55755b6
Dominik Krivohlavek <github@xkrivo.net>
2023-03-08 22:23:00 +0100

    Decompose album view

--- Shared/Views/AlbumView.swift
+++ Shared/Views/AlbumView.swift
@@ -1,5 +1,59 @@
 import SwiftUI
 
+private struct AlbumHeading: View {
+    
+    var albumName: String
+    var artistName: String
+    
+    var body: some View {
+        Image(systemName: "")
+            .resizable()
+            .frame(width: 230.0, height: 230)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(style: StrokeStyle(lineWidth: 1.0))
+            )
+        VStack {
+            Text(albumName)
+                .font(.title)
+                .bold()
+            
+            Text(artistName)
+                .font(.title2)
+        }
+    }
+}
+
+private struct AlbumActions: View {
+    var body: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Image(systemName: "play.fill")
+                Text("Play")
+            }
+            .frame(width: 120, height: 37)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(style: StrokeStyle(lineWidth: 1.0))
+            )
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "shuffle")
+                Text("Shuffle")
+            }
+            .frame(width: 120, height: 37)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(style: StrokeStyle(lineWidth: 1.0))
+            )
+        }
+    }
+}
+
 struct AlbumView: View {
     
     var albumName: String
@@ -8,48 +62,13 @@ struct AlbumView: View {
     var body: some View {
         ScrollView {
             VStack {
-                Image(systemName: "")
-                    .resizable()
-                    .frame(width: 230.0, height: 230)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10.0)
-                            .stroke(style: StrokeStyle(lineWidth: 1.0))
-                    )
-                VStack {
-                    Text(albumName)
-                        .font(.title)
-                        .bold()
-                    
-                    Text(artistName)
-                        .font(.title2)
-                }
+                AlbumHeading(
+                    albumName: "Album name",
+                    artistName: "Artist name"
+                )
                 
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "play.fill")
-                        Text("Play")
-                    }
-                    .frame(width: 120, height: 37)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(style: StrokeStyle(lineWidth: 1.0))
-                    )
-                    
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "shuffle")
-                        Text("Shuffle")
-                    }
-                    .frame(width: 120, height: 37)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(style: StrokeStyle(lineWidth: 1.0))
-                    )
-                }
-                .padding(.bottom, 30)
+                AlbumActions()
+                    .padding(.bottom, 30)
                 
                 LazyVStack {
                     ForEach(0..<15) {idx in
