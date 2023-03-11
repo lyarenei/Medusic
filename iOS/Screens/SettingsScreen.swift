853a3db60fabd7de8fa7c58f026f2a5b23723ea3
Dominik Krivohlavek <github@xkrivo.net>
2023-03-11 18:56:29 +0100

    Add server info to settings screen

--- iOS/Screens/SettingsScreen.swift
+++ iOS/Screens/SettingsScreen.swift
@@ -1,25 +1,101 @@
+import SFSafeSymbols
 import SwiftUI
+
+private struct InfoEntry: View {
+    var name: String
+    var value: String
+
+    var body: some View {
+        HStack(alignment: .center) {
+            Text(name)
+            Spacer()
+            Text(value)
+        }
+    }
+}
+
+private struct ServerInfo: View {
+    @Environment(\.api)
+    var api
+
+    @State
+    private var isOnline = false
+
+    @State
+    private var serverName = ""
+
+    @State
+    private var serverUrl = ""
+
+    @State
+    private var serverVersion = ""
+
+    var body: some View {
+        let statusText = isOnline ? "online" : "offline"
+        VStack(spacing: 5) {
+            Text("Server information")
+                .padding(.bottom, 15)
+
+            InfoEntry(name: "URL", value: serverUrl)
+            InfoEntry(name: "Name", value: serverName)
+            InfoEntry(name: "Version", value: serverVersion)
+
+            if isOnline {
+                InfoEntry(name: "Status", value: statusText)
+                    .foregroundColor(.green)
+            } else {
+                InfoEntry(name: "Status", value: statusText)
+                    .foregroundColor(.red)
+            }
+        }
+    }
+}
+
+private struct AboutSever: View {
+    @Environment(\.api)
+    var api
+
+    @State
+    private var isConnected = false
+
+    var body: some View {
+        if isConnected {
+            ServerInfo()
+                .font(.callout)
+                .padding(.leading, 15)
+                .padding(.trailing, 15)
+                .padding(.bottom, 15)
+        } else {
+            Text("Not connected to Jellyfin server")
+                .font(.subheadline)
+        }
+    }
+}
 
 struct SettingsScreen: View {
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink {
-                    AccountSettingsView()
-                } label: {
-                    Image(systemSymbol: .personCropCircle)
-                    Text("Account")
-                }
+            VStack(spacing: 10) {
+                List {
+                    NavigationLink {
+                        AccountSettingsView()
+                    } label: {
+                        Image(systemSymbol: .personCropCircle)
+                        Text("Account")
+                    }
 
-                NavigationLink {
-                    AppearanceSettingsView()
-                } label: {
-                    Image(systemSymbol: .paintbrushPointed)
-                    Text("Appearance")
+                    NavigationLink {
+                        AppearanceSettingsView()
+                    } label: {
+                        Image(systemSymbol: .paintbrushPointed)
+                        Text("Appearance")
+                    }
                 }
+                .navigationTitle("Settings")
+                .listStyle(.grouped)
+
+                AboutSever()
             }
-            .navigationTitle("Settings")
-            .listStyle(.grouped)
         }
     }
 }
@@ -28,6 +104,7 @@ struct SettingsScreen: View {
 struct SettingsScreen_Previews: PreviewProvider {
     static var previews: some View {
         SettingsScreen()
+            .environment(\.api, .preview)
     }
 }
 #endif
