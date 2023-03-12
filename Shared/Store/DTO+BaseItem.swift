5fa25e01822b4750f0105a7fea9b31a1705c549a
Dominik Krivohlavek <github@xkrivo.net>
2023-03-12 20:32:43 +0100

    Move baseitem constructors to extension

---
+++ Shared/Store/DTO+BaseItem.swift
@@ -0,0 +1,70 @@
+import Foundation
+import JellyfinAPI
+
+public extension Album {
+    init(from item: BaseItemDto) {
+        self.isDownloaded = false
+
+        if let albumId = item.id {
+            self.uuid = albumId
+        } else {
+            self.uuid = ""
+        }
+
+        if let albumName = item.name {
+            self.name = albumName
+        } else {
+            self.name = ""
+        }
+
+        if let name = item.albumArtist {
+            self.artistName = name
+        } else {
+            self.artistName = ""
+        }
+
+        if let isFavorite = item.userData?.isFavorite {
+            self.isFavorite = isFavorite
+        } else {
+            self.isFavorite = false
+        }
+
+        self.songs = []
+    }
+}
+
+public extension Song {
+    init(from item: BaseItemDto) {
+        self.isDownloaded = false
+
+        if let songId = item.id {
+            self.uuid = songId
+        } else {
+            self.uuid = ""
+        }
+
+        if let index = item.indexNumber {
+            self.index = Int(index)
+        } else {
+            self.index = 0
+        }
+
+        if let songName = item.name {
+            self.name = songName
+        } else {
+            self.name = ""
+        }
+
+        if let parentId = item.parentID {
+            self.parentId = parentId
+        } else {
+            self.parentId = ""
+        }
+
+        if let isFavorite = item.userData?.isFavorite {
+            self.isFavorite = isFavorite
+        } else {
+            self.isFavorite = false
+        }
+    }
+}
--- Shared/Store/DTOs.swift
+++ Shared/Store/DTOs.swift
@@ -7,118 +7,21 @@ public struct JellyfinServerInfo {
 }
 
 public struct Album {
-
     public var uuid: String
     public var name: String
     public var artistName: String
-    public var isDownloaded: Bool
-    public var isFavorite: Bool
-    public var songs: [Song]
+    public var isDownloaded: Bool = false
+    public var isFavorite: Bool = false
 
-    public init(
-        uuid: String,
-        name: String,
-        artistName: String,
-        isDownloaded: Bool,
-        isFavorite: Bool,
-        songs: [Song] = []
-    ) {
-        self.uuid = uuid
-        self.name = name
-        self.artistName = artistName
-        self.isDownloaded = isDownloaded
-        self.isFavorite = isFavorite
-        self.songs = songs
-    }
-
-    public init(from item: BaseItemDto) {
-        self.isDownloaded = false
-
-        if let albumId = item.id {
-            self.uuid = albumId
-        } else {
-            self.uuid = ""
-        }
-
-        if let albumName = item.name {
-            self.name = albumName
-        } else {
-            self.name = ""
-        }
-
-        if let name = item.albumArtist {
-            self.artistName = name
-        } else {
-            self.artistName = ""
-        }
-
-        if let isFavorite = item.userData?.isFavorite {
-            self.isFavorite = isFavorite
-        } else {
-            self.isFavorite = false
-        }
-
-        self.songs = []
-    }
+    // TODO: remove this and pull songs from api by their parent id
+    public var songs: [Song] = []
 }
 
-
 public struct Song {
-
     public var uuid: String
     public var index: Int
     public var name: String
     public var parentId: String
-    public var isDownloaded: Bool
-    public var isFavorite: Bool
-
-    public init(
-        uuid: String,
-        index: Int,
-        name: String,
-        parentId: String,
-        isDownloaded: Bool,
-        isFavorite: Bool
-    ) {
-        self.uuid = uuid
-        self.index = index
-        self.name = name
-        self.parentId = parentId
-        self.isDownloaded = isDownloaded
-        self.isFavorite = isFavorite
-    }
-
-    public init(from item: BaseItemDto) {
-        self.isDownloaded = false
-
-        if let songId = item.id {
-            self.uuid = songId
-        } else {
-            self.uuid = ""
-        }
-
-        if let index = item.indexNumber {
-            self.index = Int(index)
-        } else {
-            self.index = 0
-        }
-
-        if let songName = item.name {
-            self.name = songName
-        } else {
-            self.name = ""
-        }
-
-        if let parentId = item.parentID {
-            self.parentId = parentId
-        } else {
-            self.parentId = ""
-        }
-
-        if let isFavorite = item.userData?.isFavorite {
-            self.isFavorite = isFavorite
-        } else {
-            self.isFavorite = false
-        }
-    }
+    public var isDownloaded: Bool = false
+    public var isFavorite: Bool = false
 }
