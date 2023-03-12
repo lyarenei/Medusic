07a710ba7fb1732e62a2a6f9982108d640e21a1e
Dominik Krivohlavek <github@xkrivo.net>
2023-03-12 10:33:59 +0100

    Only connect to server if not in preview mode

--- Shared/JellyMusicApp.swift
+++ Shared/JellyMusicApp.swift
@@ -5,7 +5,9 @@ import JellyfinAPI
 
 @main
 struct JellyMusicApp: App {
-    private let api: API
+    private var api: API
+
+    private let PREVIEW = true
 
     init() {
         @AppStorage(SettingsKeys.serverUrl)
@@ -17,43 +19,50 @@ struct JellyMusicApp: App {
         @AppStorage(SettingsKeys.userId)
         var userId = ""
 
-        var connectUrl = URL(string: "http://localhost:8096")!
-        if let validServerUrl = URL(string: serverUrl) {
-            connectUrl = validServerUrl
-        }
-
-        let jellyfinClient = JellyfinClient(configuration: .init(
-            url: connectUrl,
-            client: "JellyMusic",
-            deviceName: UIDevice.current.model,
-            deviceID: UIDevice.current.identifierForVendor?.uuidString ?? "missing_id",
-            version: "0.0"))
-
-        Task {
-            do {
-                let resp = try await jellyfinClient.signIn(username: username , password: "aaa")
-                if let uid = resp.user?.id {
-                    userId = uid
-                }
-            } catch {
-                print("Could not log in to Jellyfin server: \(error)")
-            }
-        }
-
-        //api = .preview
-        api = API(
-            albumService: DefaultAlbumService(client: jellyfinClient),
-            songService: DefaultSongService(client: jellyfinClient),
-            imageService: DefaultImageService(client: jellyfinClient),
-            systemService: DefaultSystemService(client: jellyfinClient)
-        )
-
         // Memory image never expires.
         Kingfisher.ImageCache.default.memoryStorage.config.expiration = .never
+
         // Disk image expires in a week.
         Kingfisher.ImageCache.default.diskStorage.config.expiration = .days(7)
+
         // Limit disk cache size to 1 GB.
         Kingfisher.ImageCache.default.diskStorage.config.sizeLimit = 1000 * 1024 * 1024
+
+        api = .preview
+
+        if PREVIEW {
+            api = .preview
+        } else {
+            var connectUrl = URL(string: "http://localhost:8096")!
+            if let validServerUrl = URL(string: serverUrl) {
+                connectUrl = validServerUrl
+            }
+
+            let jellyfinClient = JellyfinClient(configuration: .init(
+                url: connectUrl,
+                client: "JellyMusic",
+                deviceName: UIDevice.current.model,
+                deviceID: UIDevice.current.identifierForVendor?.uuidString ?? "missing_id",
+                version: "0.0"))
+
+            Task {
+                do {
+                    let resp = try await jellyfinClient.signIn(username: username , password: "aaa")
+                    if let uid = resp.user?.id {
+                        userId = uid
+                    }
+                } catch {
+                    print("Could not log in to Jellyfin server: \(error)")
+                }
+            }
+
+            api = API(
+                albumService: DefaultAlbumService(client: jellyfinClient),
+                songService: DefaultSongService(client: jellyfinClient),
+                imageService: DefaultImageService(client: jellyfinClient),
+                systemService: DefaultSystemService(client: jellyfinClient)
+            )
+        }
     }
 
     var body: some Scene {
