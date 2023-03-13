ac1132fa11b718c5520767b30e7fba9ec95cc20d
Dominik Krivohlavek <github@xkrivo.net>
2023-03-13 23:13:40 +0100

    Simplify constructors

--- Shared/Store/DTOs+BaseItem.swift
+++ Shared/Store/DTOs+BaseItem.swift
@@ -3,68 +3,24 @@ import JellyfinAPI
 
 public extension Album {
     init(from item: BaseItemDto) {
+        self.uuid = item.id ?? ""
+        self.name = item.name ?? ""
+        self.artistName = item.albumArtist ?? ""
+        self.isFavorite = item.userData?.isFavorite ?? false
         self.isDownloaded = false
 
-        if let albumId = item.id {
-            self.uuid = albumId
-        } else {
-            self.uuid = ""
-        }
-
-        if let albumName = item.name {
-            self.name = albumName
-        } else {
-            self.name = ""
-        }
-
-        if let name = item.albumArtist {
-            self.artistName = name
-        } else {
-            self.artistName = ""
-        }
-
-        if let isFavorite = item.userData?.isFavorite {
-            self.isFavorite = isFavorite
-        } else {
-            self.isFavorite = false
-        }
-
+        // TODO: to be removed
         self.songs = []
     }
 }
 
 public extension Song {
     init(from item: BaseItemDto) {
+        self.uuid = item.id ?? ""
+        self.index = Int(item.indexNumber ?? 0)
+        self.name = item.name ?? ""
+        self.parentId = item.albumID ?? item.parentID ?? ""
+        self.isFavorite = item.userData?.isFavorite ?? false
         self.isDownloaded = false
-
-        if let songId = item.id {
-            self.uuid = songId
-        } else {
-            self.uuid = ""
-        }
-
-        if let index = item.indexNumber {
-            self.index = Int(index)
-        } else {
-            self.index = 0
-        }
-
-        if let songName = item.name {
-            self.name = songName
-        } else {
-            self.name = ""
-        }
-
-        if let parentId = item.albumID {
-            self.parentId = parentId
-        } else {
-            self.parentId = ""
-        }
-
-        if let isFavorite = item.userData?.isFavorite {
-            self.isFavorite = isFavorite
-        } else {
-            self.isFavorite = false
-        }
     }
 }
