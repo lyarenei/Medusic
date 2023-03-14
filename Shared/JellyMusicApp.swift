0ab4f9106fd2fb567b47ffc464c5962d16f88895
Dominik Krivohlavek <github@xkrivo.net>
2023-03-14 23:25:11 +0100

    Perform auth to avoid toggling with preview mode

--- Shared/JellyMusicApp.swift
+++ Shared/JellyMusicApp.swift
@@ -20,6 +20,13 @@ struct JellyMusicApp: App {
 
         // TODO: if starting in default mode, auth is needed, but where - needs to display error in future
         api = ApiClient(previewEnabled: Defaults[.previewMode])
+        if !Defaults[.previewMode] {
+            do {
+                try api.performAuth()
+            } catch {
+                print("Failed to perform JF auth: \(error)")
+            }
+        }
     }
 
     var body: some Scene {
