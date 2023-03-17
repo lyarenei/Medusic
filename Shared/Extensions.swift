c62724e33cb8cd96c038b745f46fee0c6c593c02
Dominik Krivohlavek <github@xkrivo.net>
2023-03-17 19:20:04 +0100

    Move settings keys to separate fiel

--- Shared/Extensions.swift
+++ Shared/Extensions.swift
@@ -1,16 +1,4 @@
-import Defaults
 import SwiftUI
-
-extension Defaults.Keys {
-    // Jellyfin settings
-    static let serverUrl = Key<String>("serverUrl", default: "")
-    static let username = Key<String>("username", default: "")
-    static let userId = Key<String>("userId", default: "")
-
-    // App settings
-    static let offlineMode = Key<Bool>("offlineMode", default: false)
-    static let previewMode = Key<Bool>("previewMode", default: false)
-}
 
 extension Array where Element == Album {
     /// Get album by specified album ID.
---
+++ Shared/Settings.swift
@@ -0,0 +1,13 @@
+import Defaults
+import Foundation
+
+extension Defaults.Keys {
+    // Jellyfin settings
+    static let serverUrl = Key<String>("serverUrl", default: "")
+    static let username = Key<String>("username", default: "")
+    static let userId = Key<String>("userId", default: "")
+
+    // App settings
+    static let offlineMode = Key<Bool>("offlineMode", default: false)
+    static let previewMode = Key<Bool>("previewMode", default: false)
+}
