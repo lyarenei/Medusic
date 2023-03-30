1cecc75c8bcca3f27b4d23e7093b066731c09c37
Dominik Krivohlavek <github@xkrivo.net>
2023-03-30 21:20:06 +0200

    Add string formatter to TimeInterval

--- Shared/Extensions.swift
+++ Shared/Extensions.swift
@@ -66,3 +66,11 @@ extension Int64 {
         return Double(self / ticksPerSecond)
     }
 }
+
+extension TimeInterval {
+    var timeString: String {
+        let minutes = Int(self) / 60
+        let seconds = Int(self) % 60
+        return String(format: "%01d:%02d", minutes, seconds)
+    }
+}
