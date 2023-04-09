6564e750264bfb61ce9d34f24a851e0ca62143fb
Dominik Krivohlavek <github@xkrivo.net>
2023-04-09 18:30:10 +0200

    Use trailing closures

--- Shared/Extensions.swift
+++ Shared/Extensions.swift
@@ -3,9 +3,9 @@ import SwiftUI
 extension Array where Element == Album {
     /// Get album by specified album ID.
     func getById(_ albumId: String) -> Album? {
-        self.first(where: { album -> Bool in
+        self.first { album -> Bool in
             album.uuid == albumId
-        })
+        }
     }
 
     var favorite: [Album] {
@@ -22,7 +22,7 @@ extension Array where Element == Song {
     /// Sorts songs by album ID, then by their order.
     /// This results in songs being grouped by their albums, and in correct order in that album.
     func sortByAlbum() -> [Song] {
-        self.sorted(by: { lhs, rhs -> Bool in
+        self.sorted { lhs, rhs -> Bool in
             // Sort by album ID, then by index
             if lhs.parentId < rhs.parentId { return true }
             if lhs.parentId > rhs.parentId { return false }
@@ -31,7 +31,7 @@ extension Array where Element == Song {
             }
 
             return false
-        })
+        }
     }
 
     /// Get only songs which belong to Album specified by its ID.
@@ -46,9 +46,9 @@ extension Array where Element == Song {
 
     /// Get song by specified song ID.
     func getById(_ songId: String) -> Song? {
-        self.first(where: { song -> Bool in
+        self.first { song -> Bool in
             song.uuid == songId
-        })
+        }
     }
 }
 
