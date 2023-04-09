e46928f8300c7bebcfe91be003c7d35529295ae8
Dominik Krivohlavek <github@xkrivo.net>
2023-04-09 14:23:13 +0200

    Simulate file change

--- Shared/Repositories/SongRepository.swift
+++ Shared/Repositories/SongRepository.swift
@@ -12,6 +12,8 @@ final class SongRepository: ObservableObject {
     init(store: Store<Song>) {
         self._songs = Stored(in: store)
         self.api = ApiClient()
+
+        print("foo")
     }
 
     /// Refresh the store data with data from service.
