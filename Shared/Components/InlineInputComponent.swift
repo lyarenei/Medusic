20f3b5c3a8ac5e1dc73a1b85e2c28d35fcf1b922
Dominik Krivohlavek <github@xkrivo.net>
2023-04-11 23:42:31 +0200

    Add inline number input component

--- Shared/Components/InlineInputComponent.swift
+++ Shared/Components/InlineInputComponent.swift
@@ -36,6 +36,33 @@ struct InlineInputComponent: View {
     }
 }
 
+struct InlineNumberInputComponent: View {
+    var labelText: String?
+    var labelSymbol: SFSymbol?
+
+    @Binding
+    var inputNumber: UInt64
+    var placeholderText = ""
+    var formatter: Formatter
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ListOptionComponent(
+                symbol: labelSymbol,
+                text: labelText
+            )
+
+            Spacer(minLength: 20)
+
+            TextField(placeholderText, value: $inputNumber, formatter: formatter)
+                .lineLimit(1)
+                .multilineTextAlignment(.trailing)
+                .frame(maxWidth: 120)
+        }
+        .keyboardType(.numberPad)
+    }
+}
+
 #if DEBUG
 struct InlineInputComponent_Previews: PreviewProvider {
     @State
@@ -44,24 +71,38 @@ struct InlineInputComponent_Previews: PreviewProvider {
     @State
     static var input2 = "some longer value"
 
+    @State
+    static var inputNumber: UInt64 = 1234
+    static var format = {
+        var fmt = NumberFormatter()
+        fmt.allowsFloats = false
+        fmt.minimum = 100
+        fmt.numberStyle = .none
+        return fmt
+    }()
+
     static var previews: some View {
-        VStack{
+        VStack {
             InlineInputComponent(
                 labelText: "Example",
                 labelSymbol: .return,
                 inputText: $input1
             )
-            .padding(.leading)
-            .padding(.trailing)
 
             InlineInputComponent(
                 labelText: "Example take 2",
                 labelSymbol: .info,
                 inputText: $input2
             )
-            .padding(.leading)
-            .padding(.trailing)
+
+            InlineNumberInputComponent(
+                labelText: "Number input and longer text",
+                labelSymbol: ._00Circle,
+                inputNumber: $inputNumber,
+                formatter: format
+            )
         }
+        .padding(.horizontal)
     }
 }
 #endif
