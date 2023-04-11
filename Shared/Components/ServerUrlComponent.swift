ba4de530cd1595da2219f7c1b2783630990f33b3
Dominik Krivohlavek <github@xkrivo.net>
2023-04-11 21:21:15 +0200

    Remove validation from server url component

--- Shared/Components/ServerUrlComponent.swift
+++ Shared/Components/ServerUrlComponent.swift
@@ -5,9 +5,6 @@ struct ServerUrlComponent: View {
     @Default(.serverUrl)
     var serverUrl: String
 
-    @State
-    var urlStatus: ServerUrlStatus = .unknown
-
     var body: some View {
         InlineInputComponent(
             labelText: "URL",
@@ -18,40 +15,6 @@ struct ServerUrlComponent: View {
         .keyboardType(.URL)
         .disableAutocorrection(true)
         .autocapitalization(.none)
-        .foregroundColor(mapStatusToColor())
-        .onChange(of: serverUrl, debounceTime: 1) { newValue in
-            urlStatus = validateUrl(newValue)
-        }
-    }
-
-    func validateUrl(_ url: String) -> ServerUrlStatus {
-        if url.isEmpty { return .unknown }
-        if let url = URL(string: url) {
-            if UIApplication.shared.canOpenURL(url) { return .valid }
-            // TODO: Check if jellyfin server
-        }
-
-        return .invalid
-    }
-
-    func mapStatusToColor() -> Color {
-        switch urlStatus {
-        case .unknown:
-            return .primary
-        case .invalid:
-            return .red
-        case .valid:
-            return .green
-        }
-    }
-
-    enum ServerUrlStatus {
-        /// Server URL has not been evaluated
-        case unknown
-        /// Server URL is invalid or does not point to Jellyfin server.
-        case invalid
-        /// Server URL is valid and points to Jellyfin server..
-        case valid
     }
 }
 
