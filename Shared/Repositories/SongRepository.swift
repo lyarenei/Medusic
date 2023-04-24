8d581d8f5260d9ea109d746c52da25898028c84d
Dominik Krivohlavek <github@xkrivo.net>
2023-04-24 22:30:39 +0200

    Clean up issues in song repo

--- Shared/Repositories/SongRepository.swift
+++ Shared/Repositories/SongRepository.swift
@@ -21,30 +21,28 @@ final class SongRepository: ObservableObject {
     func refresh(for albumId: String? = nil) async throws {
         try await apiClient.performAuth()
         if let album = albumId {
-            let remoteSongs = try await self.apiClient.services.songService.getSongs(for: album)
-            let localSongs = await self.$songs.items.filterByAlbum(id: album)
-            try await self.$songs.remove(localSongs).insert(remoteSongs).run()
+            let remoteSongs = try await apiClient.services.songService.getSongs(for: album)
+            let localSongs = await $songs.items.filterByAlbum(id: album)
+            try await $songs.remove(localSongs).insert(remoteSongs).run()
         } else {
-            let remoteSongs = try await self.apiClient.services.songService.getSongs()
-            try await self.$songs.removeAll().insert(remoteSongs).run()
+            let remoteSongs = try await apiClient.services.songService.getSongs()
+            try await $songs.removeAll().insert(remoteSongs).run()
         }
     }
 
     /// Get all songs.
     /// If an album ID is specified, return only the songs for that specified Album.
     func getSongs(ofAlbum albumId: String? = nil) async -> [Song] {
-        if let albumId = albumId {
-            return await self.$songs.items.filterByAlbum(id: albumId)
+        if let albumId {
+            return await $songs.items.filterByAlbum(id: albumId)
         }
 
-        return await self.$songs.items
+        return await $songs.items
     }
 
     /// Get a specific song form store by specified ID.
     func getSong(by songId: String) async -> Song? {
-        return await self.$songs.items.first {
-            $0.uuid == songId
-        }
+        await $songs.items.first { $0.uuid == songId }
     }
 
     /// Set/reset specified song favorite flag.
