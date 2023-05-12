9c56b9b211b2a78435474abe29a6f912607f9e63
Dominik Krivohlavek <github@xkrivo.net>
2023-05-12 22:46:01 +0200

    Refactor library screen back to vstack

--- iOS/Screens/LibraryScreen.swift
+++ iOS/Screens/LibraryScreen.swift
@@ -11,27 +11,44 @@ struct LibraryScreen: View {
 
     var body: some View {
         NavigationView {
-            List {
-                navLink(for: "Playlists", to: EmptyView(), icon: .musicNoteList)
-                    .disabled(true)
+            ScrollView {
+                VStack {
+                    mainNavigation()
+                        .padding(.leading)
 
-                navLink(for: "Artists", to: EmptyView(), icon: .musicMic)
-                    .disabled(true)
-
-                navLink(for: "Albums", to: AlbumLibraryScreen(), icon: .squareStack)
-                navLink(for: "Songs", to: SongsLibraryScreen(), icon: .musicNote)
-
-                favoritesTitle()
-                favoritesContent(albumRepo.albums.favorite.consistent)
+                    favoriteAlbums()
+                        .padding(.top, 10)
+                        .padding(.leading)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
             }
             .navigationTitle("Library")
             .navigationBarTitleDisplayMode(.large)
-            .listStyle(.plain)
             .toolbar {
                 ToolbarItem { RefreshButton(mode: .all) }
             }
         }
         .navigationViewStyle(.stack)
+    }
+
+    @ViewBuilder
+    private func mainNavigation() -> some View {
+        VStack(spacing: 5) {
+            Divider()
+            navLink(for: "Playlists", to: EmptyView(), icon: .musicNoteList)
+                .disabled(true)
+
+            Divider()
+
+            navLink(for: "Artists", to: EmptyView(), icon: .musicMic)
+                .disabled(true)
+
+            Divider()
+            navLink(for: "Albums", to: AlbumLibraryScreen(), icon: .squareStack)
+            Divider()
+            navLink(for: "Songs", to: SongsLibraryScreen(), icon: .musicNote)
+            Divider()
+        }
     }
 
     @ViewBuilder
@@ -43,31 +60,51 @@ struct LibraryScreen: View {
                     .frame(minWidth: 25)
 
                 Text(name)
+                Spacer()
+                Image(systemSymbol: .chevronRight)
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(.gray)
+                    .font(.system(size: 10))
             }
             .buttonStyle(.plain)
-            .font(.title2)
+            .padding(.trailing, 15)
         }
         .contentShape(Rectangle())
-        .frame(height: 40)
+        .font(.title2)
+        .padding(.vertical, 5)
+    }
+
+    @ViewBuilder
+    private func favoriteAlbums() -> some View {
+        VStack(spacing: 7) {
+            favoritesTitle()
+            Divider()
+            favoritesContent(albumRepo.albums.favorite.consistent)
+        }
     }
 
     @ViewBuilder
     func favoritesTitle() -> some View {
-        Text("Favorite albums")
-            .font(.title)
-            .bold()
-            .padding(.top, 30)
+        HStack {
+            Text("Favorite albums")
+                .font(.title)
+                .bold()
+
+            Spacer()
+        }
     }
 
     @ViewBuilder
     func favoritesContent(_ albums: [Album]) -> some View {
-        ForEach(albums) { album in
-            NavigationLink {
-                AlbumDetailScreen(for: album)
-            } label: {
-                AlbumListRowComponent(album: album)
-            }
-            .padding(.vertical, 7)
+        if albums.isNotEmpty {
+            AlbumCollection(albums: albums)
+                .forceMode(.asPlainList)
+                .buttonStyle(.plain)
+        } else {
+            Text("No favorite albums")
+                .font(.title3)
+                .foregroundColor(.gray)
+                .padding(.top, 10)
         }
     }
 }
@@ -75,10 +112,24 @@ struct LibraryScreen: View {
 #if DEBUG
 struct LibraryScreen_Previews: PreviewProvider {
     static var previews: some View {
-        LibraryScreen(albumRepo: .init(store: .previewStore(items: PreviewData.albums, cacheIdentifier: \.uuid)))
+        LibraryScreen(
+            albumRepo: .init(
+                store: .previewStore(
+                    items: PreviewData.albums,
+                    cacheIdentifier: \.uuid
+                )
+            )
+        )
 
-        LibraryScreen(albumRepo: .init(store: .previewStore(items: [], cacheIdentifier: \.uuid)))
-            .previewDisplayName("Empty library")
+        LibraryScreen(
+            albumRepo: .init(
+                store: .previewStore(
+                    items: [],
+                    cacheIdentifier: \.uuid
+                )
+            )
+        )
+        .previewDisplayName("Empty library")
     }
 }
 #endif
