d2f2794cf49b8b0fe2daebdce2eb652f58563691
Dominik Krivohlavek <github@xkrivo.net>
2023-05-25 21:56:13 +0200

    Clean up identifiable extensions

--- Shared/Store/DTOs+Identifiable.swift
+++ Shared/Store/DTOs+Identifiable.swift
@@ -1,22 +1,13 @@
 import JellyfinAPI
 
-// The ID getter crashes if `indexNumber: nil`,
-// so testing is needed to find out if the `indexNumber` can ever be `nil`.
-
 extension Song: Identifiable {
-    public var id: String {
-        self.uuid
-    }
+    public var id: String { uuid }
 }
 
 extension Album: Identifiable {
-    public var id: String {
-        self.uuid
-    }
+    public var id: String { uuid }
 }
 
 extension ArtistInfo: Identifiable {
-    public var id: String {
-        String(indexNumber!)
-    }
+    public var id: String { String(indexNumber!) }
 }
