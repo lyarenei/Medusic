a55b209c562df486544f2aeb8ac0ea72becca62e
Dominik Krivohlavek <github@xkrivo.net>
2023-05-26 15:58:43 +0200

    Fix linter issues in system service

--- Shared/Jellyfin/Services/SystemService/MockSystemService.swift
+++ Shared/Jellyfin/Services/SystemService/MockSystemService.swift
@@ -2,14 +2,14 @@ import Foundation
 
 final class MockSystemService: SystemService {
     func getServerInfo() async throws -> JellyfinServerInfo {
-        return JellyfinServerInfo(name: "ServerName", version: "10.x.y")
+        JellyfinServerInfo(name: "ServerName", version: "10.x.y")
     }
 
     func ping() async throws -> Bool {
-        return true
+        true
     }
 
     func logIn(username: String, password: String) async throws -> String {
-        return "logged_in"
+        "logged_in"
     }
 }
