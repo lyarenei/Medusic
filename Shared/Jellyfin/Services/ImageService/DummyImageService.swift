909d997648740e373556069082edbc63073d634a
Dominik Krivohlavek <github@xkrivo.net>
2023-05-26 16:00:41 +0200

    Fix linter issues in image service

--- Shared/Jellyfin/Services/ImageService/DummyImageService.swift
+++ Shared/Jellyfin/Services/ImageService/DummyImageService.swift
@@ -2,10 +2,11 @@ import Foundation
 
 final class DummyImageService: ImageService {
     func getImage(for itemId: String) async throws -> Data {
+        // swiftlint:disable:next force_unwrapping
         PlatformImage(named: "album1")!.pngData()!
     }
 
     func getImage(for itemId: String, size: CGSize) async throws -> Data {
-        return try await self.getImage(for: itemId)
+        try await getImage(for: itemId)
     }
 }
