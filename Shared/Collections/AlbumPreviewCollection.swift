0e9ae186f0890eb87cdb241181b189836c8e9ba6
Dominik Krivohlavek <github@xkrivo.net>
2023-06-19 22:43:52 +0200

    Refactor parts to variables

--- Shared/Collections/AlbumPreviewCollection.swift
+++ Shared/Collections/AlbumPreviewCollection.swift
@@ -23,23 +23,23 @@ struct AlbumPreviewCollection: View {
     var body: some View {
         VStack(spacing: 7) {
             Group {
-                sectionTitle()
+                sectionTitle
                 Divider()
             }
             .padding(.leading)
 
             switch stackType {
             case .vertical:
-                sectionVContent()
+                sectionVContent
                     .padding(.leading)
             case .horizontal:
-                sectionHContent()
+                sectionHContent
             }
         }
     }
 
     @ViewBuilder
-    private func sectionTitle() -> some View {
+    private var sectionTitle: some View {
         HStack {
             Text(titleText)
                 .font(.title)
@@ -48,7 +48,7 @@ struct AlbumPreviewCollection: View {
 
             Spacer()
             NavigationLink("Show all") {
-                showMoreScreen()
+                showMoreScreen
             }
             .padding(.trailing)
             .disabled(albums.isEmpty || albums.count <= limit)
@@ -56,7 +56,7 @@ struct AlbumPreviewCollection: View {
     }
 
     @ViewBuilder
-    private func sectionVContent() -> some View {
+    private var sectionVContent: some View {
         if albums.isNotEmpty {
             AlbumCollection(albums: albums.prefix(limit))
                 .forceMode(.asPlainList)
@@ -67,7 +67,7 @@ struct AlbumPreviewCollection: View {
     }
 
     @ViewBuilder
-    private func sectionHContent() -> some View {
+    private var sectionHContent: some View {
         if albums.isNotEmpty {
             ScrollView(.horizontal) {
                 LazyHStack(spacing: 20) {
@@ -92,7 +92,7 @@ struct AlbumPreviewCollection: View {
     }
 
     @ViewBuilder
-    private func showMoreScreen() -> some View {
+    private var showMoreScreen: some View {
         List {
             AlbumCollection(albums: albums)
                 .forceMode(.asList)
