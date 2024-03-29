eddb98234e8e85d45c1bcd36a3bac47809318972
Dominik Krivohlavek <github@xkrivo.net>
2024-03-29 22:41:54 +0100

    Refactor library screen

--- Screens/LibraryScreen.swift
+++ Screens/LibraryScreen.swift
@@ -1,3 +1,4 @@
+import ButtonKit
 import Defaults
 import SFSafeSymbols
 import SwiftUI
@@ -14,104 +15,122 @@ struct LibraryScreen: View {
 
     var body: some View {
         NavigationStack {
-            ScrollView {
-                content
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            List {
+                navSection
+                    .font(.system(size: 20))
+
+                Group {
+                    favoriteAlbums
+                    recentlyAdded
+                }
+                .listRowSeparator(.hidden)
+                .textCase(.none)
             }
+            .listStyle(.grouped)
             .navigationTitle("Library")
             .navigationBarTitleDisplayMode(.large)
+            .scrollContentBackground(.hidden)
             .toolbar {
-                ToolbarItem { RefreshButton(mode: .all) }
+                ToolbarItem { refreshButton }
             }
         }
     }
 
     @ViewBuilder
-    private var content: some View {
-        VStack(spacing: 15) {
-            mainNavigation
-                .padding(.leading)
-
-            favoriteAlbums
-            recentlyAddedAlbums
+    private var navSection: some View {
+        NavigationLink {} label: {
+            Label("Playlists", systemSymbol: .musicNoteList)
         }
-    }
+        .disabled(true)
 
-    @ViewBuilder
-    private var mainNavigation: some View {
-        VStack(spacing: 5) {
-            Divider()
-            navLink(for: "Playlists", to: EmptyView(), icon: .musicNoteList)
-                .disabled(true)
-
-            Divider()
-            navLink(for: "Artists", to: ArtistLibraryScreen(), icon: .musicMic)
-            Divider()
-            navLink(for: "Albums", to: AlbumLibraryScreen(albums: library.albums), icon: .squareStack)
-            Divider()
-            navLink(for: "Songs", to: SongsLibraryScreen(songs: library.songs), icon: .musicNote)
-            Divider()
+        NavigationLink {
+            ArtistLibraryScreen(artists: library.artists)
+        } label: {
+            Label("Artists", systemSymbol: .musicMic)
         }
-    }
 
-    @ViewBuilder
-    private func navLink(for name: String, to dst: some View, icon: SFSymbol) -> some View {
-        NavigationLink(destination: dst) {
-            HStack(spacing: 15) {
-                Image(systemSymbol: icon)
-                    .foregroundColor(.accentColor)
-                    .frame(minWidth: 25)
-
-                Text(name)
-                Spacer()
-                Image(systemSymbol: .chevronRight)
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(.gray)
-                    .font(.system(size: 15))
-            }
-            .buttonStyle(.plain)
-            .padding(.trailing, 15)
+        NavigationLink {
+            AlbumLibraryScreen(albums: library.albums)
+        } label: {
+            Label("Albums", systemSymbol: .squareStack)
         }
-        .contentShape(Rectangle())
-        .font(.title2)
-        .padding(.vertical, 5)
+
+        NavigationLink {
+            SongsLibraryScreen(songs: library.songs)
+        } label: {
+            Label("Songs", systemSymbol: .musicNote)
+        }
     }
 
     @ViewBuilder
     private var favoriteAlbums: some View {
-        if showFavoriteAlbums {
-            AlbumPreviewCollection(
-                for: library.albums.favorite.sorted(by: .dateAdded).ordered(by: .descending),
-                titleText: "Favorite albums",
-                emptyText: "No albums"
+        if showRecentlyAdded {
+            itemCollectionPreview(
+                title: "Favorite Albums",
+                items: library.albums.filtered(by: .favorite).sorted(by: Array<Album>.AlbumSortBy.dateAdded),
+                previewLimit: Defaults[.maxPreviewItems]
             )
-            .stackType(.horizontal)
         }
     }
 
     @ViewBuilder
-    private var recentlyAddedAlbums: some View {
+    private var recentlyAdded: some View {
         if showRecentlyAdded {
-            AlbumPreviewCollection(
-                for: library.albums.sorted(by: .dateAdded).ordered(by: .descending),
-                titleText: "Recently added",
-                emptyText: "No albums"
+            itemCollectionPreview(
+                title: "Recently added",
+                items: library.albums,
+                previewLimit: Defaults[.maxPreviewItems]
             )
-            .stackType(.horizontal)
+        }
+    }
+
+    @ViewBuilder
+    private func itemCollectionPreview(
+        title: String,
+        items: [Album],
+        previewLimit: Int
+    ) -> some View {
+        Section {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 20) {
+                    AlbumCollection(albums: items.prefix(previewLimit))
+                        .forceMode(.asTiles)
+                }
+                .padding(.leading)
+                .padding(.top)
+            }
+            .listRowInsets(EdgeInsets())
+        } header: {
+            HStack {
+                Text(title)
+                    .font(.system(size: 24))
+                    .bold()
+                    .foregroundStyle(Color.primary)
+
+                Spacer()
+                NavigationLink(value: items) {
+                    Text("View all")
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var refreshButton: some View {
+        AsyncButton {
+            try await library.refreshAll()
+        } label: {
+            Image(systemSymbol: .arrowClockwise)
+                .scaledToFit()
         }
     }
 }
 
-#if DEBUG
-struct LibraryScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        LibraryScreen()
-            .previewDisplayName("Default")
-            .environmentObject(PreviewUtils.libraryRepo)
+#Preview {
+    LibraryScreen()
+        .environmentObject(PreviewUtils.libraryRepo)
+        .environmentObject(ApiClient(previewEnabled: true))
+}
 
-        LibraryScreen()
-            .previewDisplayName("Empty library")
-            .environmentObject(PreviewUtils.libraryRepoEmpty)
     }
 }
-#endif
