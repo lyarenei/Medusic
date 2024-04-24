4bccceb86c6e760d01585b252d5559e477d409c5
Dominik Krivohlavek <github@xkrivo.net>
2024-04-24 22:51:42 +0200

    Override rect bounds for MPVolumeView

--- UI/Components/VolumeSliderComponent.swift
+++ UI/Components/VolumeSliderComponent.swift
@@ -1,10 +1,25 @@
+import SFSafeSymbols
 import SwiftUI
+
+struct VolumeSliderComponent: View {
+    var body: some View {
+        HStack(alignment: .center) {
+            Image(systemSymbol: .speaker)
+                .foregroundStyle(Color.gray)
+
+            UIVolumeSlider()
+
+            Image(systemSymbol: .speakerWave3)
+                .foregroundStyle(Color.gray)
+        }
+    }
+}
 
 // Volume slider does not work on simulator.
 
 #if targetEnvironment(simulator)
 
-struct VolumeSliderComponent: View {
+private struct UIVolumeSlider: View {
     @State
     private var value = 0.35
 
@@ -18,15 +33,26 @@ struct VolumeSliderComponent: View {
 import MediaPlayer
 import UIKit
 
-struct VolumeSliderComponent: UIViewRepresentable {
-    func makeUIView(context: Context) -> MPVolumeView {
-        let volumeView = MPVolumeView(frame: .zero)
-        // Ignore warning, we don't want the button anyway.
-        volumeView.showsRouteButton = false
-        return volumeView
+/// Volume slider that can be verically centered.
+/// From: https://stackoverflow.com/a/76360501
+private struct UIVolumeSlider: UIViewRepresentable {
+    class SystemVolumeView: MPVolumeView {
+        override func volumeSliderRect(forBounds bounds: CGRect) -> CGRect {
+            var newBounds = super.volumeSliderRect(forBounds: bounds)
+            newBounds.origin.y = bounds.origin.y
+            newBounds.size.height = bounds.size.height
+            return newBounds
+        }
     }
 
-    func updateUIView(_ view: MPVolumeView, context: Context) {}
+    func makeUIView(context: Context) -> SystemVolumeView {
+        var view = SystemVolumeView(frame: .zero)
+        view.showsVolumeSlider = true
+        view.showsRouteButton = false
+        return view
+    }
+
+    func updateUIView(_ view: SystemVolumeView, context: Context) {}
 }
 
 #endif
