65e1259791bad863ba93f751719b06c6a2782c91
Dominik Krivohlavek <github@xkrivo.net>
2024-05-02 08:45:31 +0200

    Add artist model

---
+++ Shared/Models/Artist.swift
@@ -0,0 +1,56 @@
+import Foundation
+import SwiftData
+
+@Model
+final class Artist {
+    var jellyfinId: String
+    var name: String
+    var sortName: String
+    var aboutInfo: String
+
+    var isFavorite: Bool
+    var favoriteAt: Date
+
+    var createdAt: Date
+
+    init(
+        jellyfinId: String,
+        name: String,
+        sortName: String = .empty,
+        aboutInfo: String = .empty,
+        isFavorite: Bool = false,
+        favoriteAt: Date = .distantPast,
+        createdAt: Date = .distantPast
+    ) {
+        self.jellyfinId = jellyfinId
+        self.name = name
+
+        let sortNameValue = sortName.isEmpty ? name : sortName
+        self.sortName = sortNameValue.lowercased()
+
+        self.aboutInfo = aboutInfo
+        self.isFavorite = isFavorite
+        self.favoriteAt = favoriteAt
+        self.createdAt = createdAt
+    }
+}
+
+extension Artist {
+    static func predicate(for option: FilterOption) -> Predicate<Artist> {
+        switch option {
+        case .all:
+            return #Predicate<Artist> { _ in true }
+        case .favorite:
+            return #Predicate<Artist> { $0.isFavorite }
+        }
+    }
+}
+
+extension Artist {
+    // SwiftData does not support derived attributes yet, so we need to do this.
+    var runtime: TimeInterval {
+        var runtime: TimeInterval = 0
+//        albums.forEach { runtime += $0.runtime }
+        return runtime
+    }
+}
