be3c3257e629edcbd7c70c64420795aef1e01d7f
Dominik Krivohlavek <github@xkrivo.net>
2024-06-07 23:54:21 +0200

    Add method for favoriting artist

--- Repositories/Library/LibraryRepository+Artists.swift
+++ Repositories/Library/LibraryRepository+Artists.swift
@@ -8,11 +8,28 @@ extension LibraryRepository {
     }
 
     /// Set favorite flag for artist both locally and in Jellyfin.
+    @available(*, deprecated, message: "Use method which accepts ID")
     func setFavorite(artist: ArtistDto, isFavorite: Bool) async throws {
         guard var newArtist = await artists.by(id: artist.id) else { throw LibraryError.notFound }
         try await apiClient.services.mediaService.setFavorite(itemId: artist.id, isFavorite: isFavorite)
         newArtist.isFavorite = isFavorite
         try await $artists.insert(newArtist)
+    }
+
+    /// Set favorite flag for artist both locally and in Jellyfin.
+    func setFavorite(artistId: String, isFavorite: Bool) async {
+        do {
+            guard var artist = await artists.by(id: artistId) else { throw LibraryError.notFound }
+            try await apiClient.services.mediaService.setFavorite(itemId: artistId, isFavorite: isFavorite)
+            artist.isFavorite = isFavorite
+            try await $artists.insert(artist)
+        } catch let error as LibraryError {
+            logger.warning("Failed to update favorite status: \(error.localizedDescription)")
+            Alerts.error("Action failed", reason: error.localizedDescription)
+        } catch {
+            logger.warning("Failed to update favorite status: \(error.localizedDescription)")
+            Alerts.error("Action failed")
+        }
     }
 
     /// Get total runtime of all songs/albums for specified artist.
