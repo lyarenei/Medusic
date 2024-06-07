14aa6eadb188b38a2234367135a64e3d488c5761
Dominik Krivohlavek <github@xkrivo.net>
2024-06-07 23:32:52 +0200

    Remove layout

--- UI/Buttons/FavoriteButton.swift
+++ UI/Buttons/FavoriteButton.swift
@@ -12,7 +12,6 @@ struct FavoriteButton: View {
     private let item: any JellyfinItem
     private let textFavorite: String?
     private let textUnfavorite: String?
-    private var layout: ButtonLayout = .horizontal
 
     init(
         item: any JellyfinItem,
@@ -30,11 +29,12 @@ struct FavoriteButton: View {
         AsyncButton {
             await action()
         } label: {
-            switch layout {
-            case .horizontal:
-                hLayout(symbol, text)
-            case .vertical:
-                vLayout(symbol, text)
+            if let text {
+                Label(text, systemSymbol: symbol)
+            } else {
+                Image(systemSymbol: symbol)
+                    .resizable()
+                    .scaledToFit()
             }
         }
         .sensoryFeedback(.success, trigger: isFavorite) { old, new in !old && new }
@@ -46,36 +46,6 @@ struct FavoriteButton: View {
     enum ButtonLayout {
         case horizontal
         case vertical
-    }
-
-    @ViewBuilder
-    private func hLayout(_ symbol: SFSymbol, _ text: String?) -> some View {
-        HStack {
-            icon(symbol)
-            buttonText(text)
-        }
-    }
-
-    @ViewBuilder
-    private func vLayout(_ symbol: SFSymbol, _ text: String?) -> some View {
-        VStack {
-            icon(symbol)
-            buttonText(text)
-        }
-    }
-
-    @ViewBuilder
-    private func buttonText(_ text: String?) -> some View {
-        if let text {
-            Text(text)
-        }
-    }
-
-    @ViewBuilder
-    private func icon(_ symbol: SFSymbol) -> some View {
-        Image(systemSymbol: symbol)
-            .scaledToFit()
-            .foregroundColor(.red)
     }
 
     @MainActor
@@ -101,31 +71,10 @@ struct FavoriteButton: View {
     }
 }
 
-extension FavoriteButton {
-    func setLayout(_ layout: ButtonLayout) -> FavoriteButton {
-        var view = self
-        view.layout = layout
-        return view
-    }
-}
-
 #if DEBUG
 struct FavoriteButton_Previews: PreviewProvider {
     static var previews: some View {
         FavoriteButton(item: PreviewData.album)
-            .previewDisplayName("Icon only")
-            .font(.title)
-            .environmentObject(PreviewUtils.libraryRepo)
-
-        FavoriteButton(item: PreviewData.album, textUnfavorite: "Test")
-            .setLayout(.horizontal)
-            .previewDisplayName("Horizontal")
-            .font(.title)
-            .environmentObject(PreviewUtils.libraryRepo)
-
-        FavoriteButton(item: PreviewData.album, textUnfavorite: "Test")
-            .setLayout(.vertical)
-            .previewDisplayName("Vertical")
             .font(.title)
             .environmentObject(PreviewUtils.libraryRepo)
     }
